4b50b6de4253299ebef49e0ace81e0e13a1b31b7
Advaith Manoj <89682610+Advaith-dev@users.noreply.github.com>
2024-10-30 09:13:35 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-import cv2
 from flask import Flask, request, jsonify
-from PIL import Image
 from ultralytics import YOLO
+from PIL import Image
 import io
+import numpy as np
+import base64
 
+# Initialize the Flask app
 app = Flask(__name__)
 
-# Load the PyTorch YOLO model
-model = YOLO("best.pt")
+# Load the YOLO model (YOLOv5 in this case)
+model = YOLO("best.pt")  
 
-
+# Endpoint to process the uploaded image
 @app.route('/predict', methods=['POST'])
-def predict():
+def detect_objects():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+    
+    # Read the uploaded image
     file = request.files['image']
     img = Image.open(io.BytesIO(file.read()))
 
-    results = model.predict(Image, conf=0.6, imgsz=640)
+    # Run YOLO object detection
+    results = model.predict(img, conf=0.6, imgsz=640)
+    
+    # Get the annotated frame
     detected_objects = [model.names[int(cls)] for cls in results[0].boxes.cls]
-    return detected_objects
+    
+    conf = results[0].boxes.conf
+    
+    confidence_value = conf.item()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return jsonify({"detection": detected_objects[0] ,"conf": confidence_value})
+
+# Main function to run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
